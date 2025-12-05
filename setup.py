5a356f029c3711f220a2299483df9c6489b4d46f
Ja9035i <chandranjai66@gmail.com>
2025-12-05 22:15:33 +0530

    requriment folder

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+__version__ = "0.0.0"
+
+REPO_NAME = "chicken-disease-prediction"
+AUTHOR_USER_NAME = "ja9035i"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "chandranjai66@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small package for CNN based chicken disease prediction",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
